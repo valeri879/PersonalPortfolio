e91d77ea37521c96ee9bf0ecc93d0d9b3751cac1
valeri879 <valeri.kharitonashvili1@gmail.com>
2016-11-14 17:50:13 +0400

    db

---
+++ assets/db/personalportfolio.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2016 at 02:49 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `personalportfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `from_year` text NOT NULL,
+  `to_year` text NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `school` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_detail`
+--
+
+CREATE TABLE `personal_detail` (
+  `title` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills_knowledge`
+--
+
+CREATE TABLE `skills_knowledge` (
+  `bar_title` text NOT NULL,
+  `1_bar` text NOT NULL,
+  `1_bar_percent` int(10) NOT NULL,
+  `2_bar` text NOT NULL,
+  `2_bar_percent` int(10) NOT NULL,
+  `3_bar` text NOT NULL,
+  `3_bar_percent` int(11) NOT NULL,
+  `4_bar` text NOT NULL,
+  `4_bar_percent` int(11) NOT NULL,
+  `5_bar` text NOT NULL,
+  `5_bar_percent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(100) NOT NULL,
+  `user` text NOT NULL,
+  `profession` text NOT NULL,
+  `birth` date NOT NULL,
+  `adress` text NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_experience`
+--
+
+CREATE TABLE `work_experience` (
+  `from_year` text NOT NULL,
+  `to_year` text NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `company` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
