d59fd0b2e573bb2420ec103477d49b9b18a4a9ad
valeri879 <valeri.kharitonashvili1@gmail.com>
2016-11-18 14:09:09 +0400

    Test conflict

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -52,7 +52,7 @@ class Admin extends CI_Controller {
 		}
 	}
 
-	public function experience() 								//experience function
+	public function experience() 								//experience function___
 	{
 		$this->render('experience');
 	}
